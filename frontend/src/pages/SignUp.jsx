414ef7ad28d94addcb025d06738fd315e845563e
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-26 23:17:54 +0800

    sign up page tabs added

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import classes from './SignUp.module.css';
 
 const SignUp = () => {
-  return (
-    <div>
-      Sign Up Page
-    </div>
-  )
-}
+  const [isOrganisation, setIsOrganisation] = useState(false);
 
-export default SignUp
+  const setOrganisation = () => {
+    setIsOrganisation(true);
+  };
+
+  const setUser = () => {
+    setIsOrganisation(false);
+  };
+
+  return (
+    <div className={classes.signupContainer}>
+      <div className={classes.tabs}>
+        <div
+          className={`${classes.tab} ${!isOrganisation ? classes.active : ''}`}
+          onClick={setUser}
+        >
+          User
+        </div>
+        <div
+          className={`${classes.tab} ${isOrganisation ? classes.active : ''}`}
+          onClick={setOrganisation}
+        >
+          Organisation
+        </div>
+      </div>
+      <div className={classes.content}>
+        {isOrganisation ? (
+          <div>Is organisation</div>
+        ) : (
+          <div>Is not organisation</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
