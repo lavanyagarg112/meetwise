1e0a6cbd5ec5d3ea1af86675576391ab207850ce
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-26 23:30:04 +0800

    navigation made better

--- frontend/src/components/Layout/Navigation.jsx
+++ frontend/src/components/Layout/Navigation.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import classes from './Layout.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navigation = () => {
 
   const [menuIsOpen, setIsMenuOpen] = useState(true)
 
+  const location = useLocation()
+
   const toggleMenuHandler = () => {
     setIsMenuOpen(prevState => !prevState)
   }
+
+  const isActive = (path) => location.pathname === path;
 
   return (
     <header className={classes.navigation}>
@@ -21,13 +25,13 @@ const Navigation = () => {
       </div>
       <nav className={`${classes.navItems} ${menuIsOpen ? classes.show : ''}`}>
         <div className={classes.logo}><Link to="/" className={classes.text}>Meet Space</Link></div>
-        <div className={classes.navItem}>
+        <div className={`${classes.navItem} ${isActive('/') ? classes.active : ''}`}>
           <Link to="/" className={classes.text}>Home</Link>
         </div>
-        <div className={classes.navItem}>
+        <div className={`${classes.navItem} ${isActive('/about') ? classes.active : ''}`}>
           <Link to="/about" className={classes.text}>About Us</Link>
         </div>
-        <div className={classes.navItem}>
+        <div className={`${classes.navItem} ${isActive('/sign-up') ? classes.active : ''}`}>
           <Link to="/sign-up" className={classes.text}>Sign Up</Link>
         </div>
       </nav>
