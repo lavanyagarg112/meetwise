1358e5d41606d1064236a7772d02c4b8bf67a7a2
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-26 22:12:43 +0800

    basic layout created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
+
 import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <Layout>
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/about' element={<AboutUs />} />
         </Routes>
     </Layout>
 
--- frontend/src/components/Layout/Navigation.jsx
+++ frontend/src/components/Layout/Navigation.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Layout.module.css'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
+
+  const [menuIsOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenuHandler = () => {
+    setIsMenuOpen(prevState => !prevState)
+  }
+
   return (
-    <div className={classes.navigation}>
-      Navigation
-    </div>
+    <header className={classes.navigation}>
+      <div className={classes.hamburger} onClick={toggleMenuHandler}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`${classes.navItems} ${menuIsOpen ? classes.show : ''}`}>
+        <div className={classes.logo}><Link to="/" className={classes.text}>Meet Space</Link></div>
+        <div className={classes.navItem}>
+          <Link to="/" className={classes.text}>Home</Link>
+        </div>
+        <div className={classes.navItem}>
+          <Link to="/about" className={classes.text}>About Us</Link>
+        </div>
+        <div className={classes.navItem}>
+          <Link to="/sign-up" className={classes.text}>Sign Up</Link>
+        </div>
+      </nav>
+    </header>
   )
 }
 
