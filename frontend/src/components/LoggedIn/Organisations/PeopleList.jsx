60cdc0f88c7d55d0e9323c488e1415bb7e12263e
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-06-27 15:56:45 +0800

    basic design created except invite user

--- frontend/src/components/LoggedIn/Organisations/PeopleList.jsx
+++ frontend/src/components/LoggedIn/Organisations/PeopleList.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import classes from './PeopleList.module.css'
 
-const PeopleList = ({people, currentUser}) => {
+const PeopleList = ({ people, currentUser }) => {
   return (
-    <div>
-      {!people || people.length === 0 ? <div>None Yet.</div> : (
-
-        people.map((person) =>
-            <div>
-                <div>{person.username}</div>
-                {person.id === currentUser.id ? <div>My Profile</div> : <div>View Profile</div>}
+    <div className={classes.peopleListContainer}>
+      {!people || people.length === 0 ? (
+        <div className={classes.noPeopleMessage}>None Yet.</div>
+      ) : (
+        people.map((person) => (
+          <div key={person.id} className={classes.personContainer}>
+            <div className={classes.personName}>{person.username}</div>
+            <div className={classes.profileLink}>
+              {person.id === currentUser.id ? 'My Profile' : 'View Profile'}
             </div>
-        )
-
+          </div>
+        ))
       )}
     </div>
   )
