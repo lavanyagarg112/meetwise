7ee41c6151371f058f20f64a02f9597a537dda31
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-07-04 00:35:43 +0800

    backend dot issue

--- backend/OrganisationHelpers.py
+++ backend/OrganisationHelpers.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from backend.Enums import Roles
-from backend.Errors import AuthenticationError
-from backend.IOSchema import Organisation, Meeting, Person
-from backend.database import mapOrgNameToID, mapOrgIDToName, mapTeamNameToId, getUserOrgs, getBulkUsersByIds, \
+from Enums import Roles
+from Errors import AuthenticationError
+from IOSchema import Organisation, Meeting, Person
+from database import mapOrgNameToID, mapOrgIDToName, mapTeamNameToId, getUserOrgs, getBulkUsersByIds, \
     getAdminsOrg, getUsersOrg, getOrgRoleByID, getTeamRoleByID, getAdminsTeam, getUsersTeam, getOutsideTeam, getAll, \
     getUserDetailsByEmail, getInvites
 
