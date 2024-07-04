9ee9232a0c1d3a5e6208cebf54048a2d594dc6bd
Kimaya Wanjari <kimaya.wanjari@nuscomputing.com>
2024-07-05 02:03:38 +0800

    task assignee removed + uncommon words generation

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,8 @@ import time
 from audio_transcription import transcribe
 import Meeting
 
-file_path = "backend/testFiles/meetingTest1.mp3"
+
+file_path = "backend/testFiles/meetingTest2.mp3"
 file_size = os.path.getsize(file_path)
 
 start = time.time()
@@ -17,30 +18,44 @@ end_read = time.time()
 transcription_text = transcribe(file_obj)
 end_transcription = time.time()
 
+with open("backend/testFiles/transcription.txt", "w") as t_file:
+    t_file.write(transcription_text)
+
 transcription_size = len(transcription_text)
+
 meeting = Meeting.Meeting(transcription_text)
+
+uncommon_words = meeting.generate_uncommon_words()
+end_uncommon_words = time.time()
 
 summary = meeting.generate_summary()
 end_summary = time.time()
+
 
 todo = meeting.generate_todo()
 end_todo = time.time()
 
 report_content = (
+    
     f"mp3 file size: {file_size / (1024 * 1024):.2f} MB\n"
     f"Transcription size: {int(transcription_size)} characters\n\n"
+    f"------------------------------------------------------------------------------------------------------------------------\n"
+    f"+ mp3 file read time: {end_read - start:.2f} seconds\n"
+    f"| Transcription time: {end_transcription - end_read:.2f} seconds\n"
+    f"| Uncommon words generation time: {end_uncommon_words - end_transcription:.2f} seconds\n"
+    f"| Summary generation time: {end_summary - end_uncommon_words:.2f} seconds\n"
+    f"| To-do generation time: {end_todo - end_summary:.2f} seconds\n"
+    f"+ Total time taken: {end_todo - start:.2f} seconds\n"
+    f"------------------------------------------------------------------------------------------------------------------------\n\n"
+    
 
-    f"mp3 file read time: {end_read - start:.2f} seconds\n"
-    f"Transcription time: {end_transcription - end_read:.2f} seconds\n"
-    f"Summary generation time: {end_summary - end_transcription:.2f} seconds\n"
-    f"To-do generation time: {end_todo - end_summary:.2f} seconds\n"
-    f"Total time taken: {end_todo - start:.2f} seconds\n\n\n"
-
+    f"Uncommon Words List:\n{uncommon_words}\n\n"
     f"Summary:\n{summary}\n\n"
     f"To-do List:\n{todo}\n\n"
 )
 
 with open("backend/testFiles/REPORT.txt", "w") as report_file:
     report_file.write(report_content)
+
 
 print("Report written to REPORT.txt")
--- backend/audio_transcription.py
+++ backend/audio_transcription.py
@@ -43,5 +43,3 @@ def transcribe(file_obj):
     transcription_text = get_transcription_result(transcription_id, headers)
 
     return transcription_text
-
-#dummy commit by Sarthak
