bbbf1cb9bb8528a32bbbcc6bab8196205c40c603
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-07-06 21:55:50 +0800

    making dashboard

--- frontend/src/components/Home/HomeUser.jsx
+++ frontend/src/components/Home/HomeUser.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 
 const HomeUser = ({user}) => {
+
+
+  const getAllTodos = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/get-user-todos`, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include' 
+        })
+
+        if (!response.ok) {
+          throw new Error('An error occurred fetching the todos.');
+        }
+
+        const data = await response.json()
+
+      } catch (error) {
+        console.log("error: ", error)
+      }
+  }
+
   return (
+
     <div>
-        {user.firstName}
-        <div>Lastest Meeting????</div>
-        <div>User todos</div>
-        <div>Some links maybe</div>
+        {user.firstName} {user.lastName}
+        <div>Show todo calendar</div>
+        <div>Upcoming tasks</div>
+        <div>Direct link to go to organisations page</div>
     </div>
   )
 }
